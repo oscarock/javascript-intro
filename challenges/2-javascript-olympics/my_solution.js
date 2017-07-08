1641728745a071fed43976798efab28dc168a85d
oscar diaz <you@example.com>
2017-07-08 13:51:56 -0500

    temp

--- challenges/2-javascript-olympics/my_solution.js
+++ challenges/2-javascript-olympics/my_solution.js
@@ -4,15 +4,32 @@
 
 
 // 0. Objetos
+var nombre1 = {
+	name: "oscar",
+	age: 26,
+	height: 1.75
+}
 
-
+var nombre2 = {
+	name: "maria",
+	age: 29,
+	height: 1.45
+}
 
 // 1. Añadir propiedades a objetos
-
-
+function addWinToAthlete(athletes, event){
+	athletes.forEach(function(athlete){
+		athlete.win = function(){
+			console.log(athlete.name + "won the" + event)
+		}
+	})
+}
 
 // Function Declaration
-
+function reverseString(string){
+	//logica para volterar
+	return result
+}
 
 
 // 2. Voltea un String
@@ -21,12 +38,20 @@
 
 
 // 3. Remueve numeros impares
-
-
+var arr = [1,2,3,4,5]
+function removeOdds(arr){
+	return arr.filter(function(element){
+		element % 2 === 0
+	}
+}
 
 
 // 4. Funcion constructora
-
+function Athlete(name,age,sport){
+this.name
+this.age
+this.sport
+} 
 
 
 
