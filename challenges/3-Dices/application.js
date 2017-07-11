38eb0719a16c639d27199538d1e1198775b2fa11
Your Name <you@example.com>
2017-07-11 12:28:34 -0500

    solucion reto 3

--- challenges/3-Dices/application.js
+++ challenges/3-Dices/application.js
@@ -1,16 +1,60 @@
-$(document).ready(function() {  // Codigo Procedural Procedimental --> OOP
-  // Event Listener
-  $('#roller button.add').on('click', function() { // Controlador
-    $('.dice').append('<div class="die">0</div>'); // Vista
-  });
+// $(document).ready(function() {  // Codigo Procedural Procedimental --> OOP
+//   // Event Listener
+//   $('#roller button.add').on('click', function() { // Controlador
+//     $('.dice').append('<div class="die">0</div>'); // Vista
+//   });
+//
+//   // Event listener
+//   $('#roller button.roll').on('click', function() { // Controlador
+//     $('.die').each(function(index, dieHtml) { // Controlador
+//       var value = Math.floor((Math.random()*6)+1); // Model
+//       $(dieHtml).text(value); // Vista
+//     });
+//   });
+// });
 
-  // Event listener
-  $('#roller button.roll').on('click', function() { // Controlador
-    $('.die').each(function(index, dieHtml) { // Controlador
-      var value = Math.floor((Math.random()*6)+1); // Model
-      $(dieHtml).text(value); // Vista
+
+//mi solucion
+function Controller(model, view) {
+    this.model = model
+    this.view = view
+}
+
+function Model(){}
+
+function View(){}
+
+Controller.prototype.event_botton_add = function() {
+    var controller = this
+    // console.log(this)
+    $('#roller button.add').on('click', function() {
+        controller.view.content_add_button()
     });
-  });
-});
+}
+
+Controller.prototype.event_botton_roll = function(){
+    var controller = this
+    // console.log(this)
+    $('#roller button.roll').on('click', function() {
+        // console.log(controller.model.roll_die())
+        $('.die').each(function(index, dieHtml) {
+          var value = controller.model.roll_die
+          $(dieHtml).text(value);
+        })
+    })
+}
+
+View.prototype.content_add_button = function(){
+    $('.dice').append('<div class="die">0</div>')
+    // console.log('entre')
+}
+
+Model.prototype.roll_die = function (){
+    return Math.floor((Math.random()*6)+1)
+    // console.log('entre')
+}
 
 
+var play_game = new Controller(new Model(), new View())
+play_game.event_botton_add()
+play_game.event_botton_roll()
