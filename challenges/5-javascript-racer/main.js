784f9049c2e21018f8f658fe5020cc55a933f4b6
oscar diaz <you@example.com>
2017-07-12 21:06:51 -0500

    reto javascript 5 en clase :)

--- challenges/5-javascript-racer/main.js
+++ challenges/5-javascript-racer/main.js
@@ -7,31 +7,58 @@ function Model(){}
 
 function View(){}
 
+
+
+
 Controller.prototype.event_keyboard_player1 = function() {
 	var a = 1
 	$(document).keyup(function(event){
 	    if (event.keyCode == 80) {
-			// console.log($('#player1_strip td').hasClass('inactive active'))
-			// if($('#player1_strip td').hasClass('inactive') === 'inactive') {
-			// 	$('#player1_strip td').removeClass('inactive')
-			// }
-
-			// console.log(a++)
-			// console.log('valor' + a);
-			// console.log($('.racer_table td').attr('class'))
-			// console.log($('#player1_strip td').hasClass('inactive'))
-			// if ($('#player1_strip td').hasClass('inactive')) {
-			// 	$('#player1_strip td').eq(a++).removeClass('inactive')
-			// }
+			if($('#player1_strip td').last().hasClass('active')){
+				alert('gano player 1')
+			}
 			$('#player1_strip').find('td').eq(a++).addClass(function(){
-				$(this).addClass('active')
-			})
-			// console.log($('#player1_strip td').eq(a).removeClass('active'))
+					$(this).addClass('active')
+				})
+
+			$(".active").prev().removeClass('active')
 	    }
 	});
 }
 
+Controller.prototype.event_keyboard_player2 = function() {
+	var a = 1
+	$(document).keyup(function(event){
+	    if (event.keyCode == 81) {
+			if($('#player2_strip td').last().hasClass('active')){
+				alert('gano player 2')
+			}
 
+			$('#player2_strip').find('td').eq(a++).addClass(function(){
+				$(this).addClass('active')
+			})
+
+			$(".active").prev().removeClass('active')
+		}
+	});
+}
+
+Controller.prototype.button_restard = function(){
+	$('#restart').on('click',function(){
+		location.reload();
+	})
+}
+
+Controller.prototype.time_out = function(){
+	setTimeout(function(){ $('#time').html("2") }, 1000);
+	setTimeout(function(){ $('#time').html("1") }, 2000);
+	setTimeout(function(){ $('.racer_table').show() }, 3000);
+	setTimeout(function(){ $('#time').html("GOOO!!!") }, 3000);
+	setTimeout(function(){ $('#restart').show() }, 4000);
+}
 
 var play = new Controller()
 play.event_keyboard_player1()
+play.event_keyboard_player2()
+play.button_restard()
+play.time_out()
